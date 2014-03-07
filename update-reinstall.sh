71f78db978935d3ca7e9cf2a56afc717ecbf0a1c
Mstrodl <Ipadlover8322@gmail.com>
2014-03-06 19:59:04 -0500

    Delete update-reinstall.sh

--- update-reinstall.sh
+++
@@ -1,5 +0,0 @@
-echo "Welcome To The MCEdinux Updater/Re-Installer"
-sudo echo "Updating/Re-Installing"
-rm ~/mcedit
-git clone --recursive https://github.com/mcedit/mcedit
-echo "Update/Re-Install Completed!"
