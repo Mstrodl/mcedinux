139297d3f9079d92b687b5e41e40de2ecec7b8cb
Mstrodl <Ipadlover8322@gmail.com>
2014-03-07 08:44:44 -0500

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-echo "to uninstall dependencies type in your password if you would like to keep them dont type in anything and press enter three times"
-sudo apt-get remove git python-opengl python-pygame python-yaml python-numpy
-rmdir ~/mcedit
-rmdir ~/Desktop/MCEdit
-echo "uninstallation completed!"
