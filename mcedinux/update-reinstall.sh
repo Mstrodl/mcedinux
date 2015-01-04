7ab43e35c968f75c7bccea4c5ca00e813452260c
Mstrodl <Ipadlover8322@gmail.com>
2015-01-04 16:14:38 -0500

    more standard commands

--- mcedinux/update-reinstall.sh
+++ mcedinux/update-reinstall.sh
@@ -1,5 +1,5 @@
 echo "Welcome To The MCEdinux Updater/Re-Installer"
 sudo echo "Updating/Re-Installing"
-rmdir ~/mcedit
+rm -rf ~/mcedit
 git clone --recursive https://github.com/mcedit/mcedit
 echo "Update/Re-Install Completed!"
