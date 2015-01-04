ac45c8970df34027d05d61bc3849051cb09c573f
Mstrodl <Ipadlover8322@gmail.com>
2015-01-04 16:16:27 -0500

    Update uninstall.sh

--- mcedinux/uninstall.sh
+++ mcedinux/uninstall.sh
@@ -1,5 +1,5 @@
-echo "to uninstall dependencies type in your password if you would like to keep them dont type in anything and press enter three times"
-sudo apt-get remove git python-opengl python-pygame python-yaml python-numpy
-rmdir ~/mcedit
-rm ~/Desktop/MCEdit
+echo "to uninstall dependencies type in your password if you would like to keep them dont type in anything and press enter"
+sudo -p "Password" apt-get remove git python-opengl python-pygame python-yaml python-numpy
+rm -rf ~/mcedit
+rm ~/Desktop/MCEdit.dekstop
 echo "uninstallation completed!"
